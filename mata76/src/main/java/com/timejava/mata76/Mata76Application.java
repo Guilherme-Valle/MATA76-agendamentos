ec3ef33829b158dbb05652772043e7a851c8863b
rafaelmendes1 <rarafafa3245@gmail.com>
2022-11-17 17:47:07 -0300

    Adiciona integração com Firebase

--- mata76/src/main/java/com/timejava/mata76/Mata76Application.java
+++ mata76/src/main/java/com/timejava/mata76/Mata76Application.java
@@ -1,13 +1,37 @@
 package com.timejava.mata76;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Mata76Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Mata76Application.class, args);
+		ClassLoader classLoader = Mata76Application.class.getClassLoader();
+
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+		FileInputStream serviceAccount =
+			new FileInputStream(file.getAbsolutePath());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			.setDatabaseUrl("https://mata76-agendamento-default-rtdb.firebaseio.com")
+			.build();
+
+
+		if(FirebaseApp.getApps().isEmpty()) {
+			FirebaseApp.initializeApp(options);
+		}
 	}
 
 }
