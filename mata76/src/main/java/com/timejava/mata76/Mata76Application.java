9b1c85bafcedf4c8c0827d0d55aac941ea0259d6
rafaelmendes1 <rarafafa3245@gmail.com>
2022-11-17 12:03:00 -0300

    Adicionando workspace sprimg starter

---
+++ mata76/src/main/java/com/timejava/mata76/Mata76Application.java
@@ -0,0 +1,13 @@
+package com.timejava.mata76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mata76Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mata76Application.class, args);
+	}
+
+}
---
+++ mata76/src/test/java/com/timejava/mata76/Mata76ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timejava.mata76;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mata76ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
